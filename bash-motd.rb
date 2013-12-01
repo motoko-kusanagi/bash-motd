7adf8023918dfeed46e2350469d1eb9fadbcfbce
Marcin Szymkowski <mszymkowski@gmail.com>
2013-12-01 21:54:27 +0100

    added case statement

--- bash-motd.rb
+++ bash-motd.rb
@@ -3,23 +3,30 @@
 require 'open-uri'
 require 'nokogiri'
 
-bash = Nokogiri::HTML(open("http://bash.org.pl/random"))
-bash = bash.css("div[class='quote post-content post-body']").text
+def bash_pl()
+  bash = Nokogiri::HTML(open("http://bash.org.pl/random"))
+  bash = bash.css("div[class='quote post-content post-body']").text
 
-bash = bash.gsub("\n","").gsub("\t","")
-if bash.include? "\r"
-  bash = bash.split("\r")
-  bash.each do |line|
-    if !line.empty?
-      puts line
+  bash = bash.gsub("\n","").gsub("\t","")
+  if bash.include? "\r"
+    bash = bash.split("\r")
+    bash.each do |line|
+      if !line.empty?
+        puts line
+      end
     end
-  end
-else
-  bash = bash.split("<")
-  bash.each do |line|
-    if !line.empty?
-      puts "<#{line}"
+  else
+    bash = bash.split("<")
+    bash.each do |line|
+      if !line.empty?
+        puts "<#{line}"
+      end
     end
   end
 end
 
+arg = ARGV[0]
+
+option = case arg
+  when "bash_pl" then bash_pl
+end
