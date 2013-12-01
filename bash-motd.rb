75d36fbaccb659d5e36655fb0785031648eee972
Marcin Szymkowski <mszymkowski@gmail.com>
2013-12-01 12:40:49 +0100

    added reading bash.org.pl/random quotes

--- bash-motd.rb
+++ bash-motd.rb
@@ -1,3 +1,25 @@
 #!/usr/bin/ruby
 
+require 'open-uri'
 require 'nokogiri'
+
+bash = Nokogiri::HTML(open("http://bash.org.pl/random"))
+bash = bash.css("div[class='quote post-content post-body']").text
+
+bash = bash.gsub("\n","").gsub("\t","")
+if bash.include? "\r"
+  bash = bash.split("\r")
+  bash.each do |line|
+    if !line.empty?
+      puts line
+    end
+  end
+else
+  bash = bash.split("<")
+  bash.each do |line|
+    if !line.empty?
+      puts "<#{line}"
+    end
+  end
+end
+
